007c9035020180e7949d915acba0c299c5847ede
Miguel E Uicab Canabal <devmigueluicab@gmail.com>
2024-11-17 00:33:37 -0600

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import animations from '@midudev/tailwind-animations'
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -15,8 +17,22 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                red: {
+                    50: '#ffe5e5',
+                    100: '#ffcccc',
+                    200: '#ff9999',
+                    300: '#ff6666',
+                    400: '#ff3333',
+                    500: '#FF0000',
+                    600: '#e60000',
+                    700: '#b30000',
+                    800: '#800000',
+                    900: '#4d0000',
+                },
+            }
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, animations],
 };
