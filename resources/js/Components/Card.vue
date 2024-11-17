274cabc6d2882f1074b5818ea3e9bfd6d40a9149
Miguel E Uicab Canabal <devmigueluicab@gmail.com>
2024-11-17 00:31:28 -0600

    Update components

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const props = defineProps({
+	title: {
+		type: String,
+		default: ''
+	}
+});
+</script>
+
+<template>
+	<section class="content-center">
+		<div
+			class="bg-white rounded-lg shadow-md overflow-hidden h-full transition-all duration-300 ease-in-out transform hover:scale-[1.05] hover:shadow-md flex flex-col py-4 px-6"
+		>
+			<div class="mb-2">
+				<h3 class="uppercase font-bold  text-center">
+					{{ props.title }}
+				</h3>
+			</div>
+			<div class="h-full">
+				<slot/>
+			</div>
+		</div>
+	</section>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -1,18 +1,20 @@
-<script setup lang="ts">
+<script setup>
 import { computed, onMounted, onUnmounted, watch } from 'vue';
 
-const props = withDefaults(
-    defineProps<{
-        show?: boolean;
-        maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-        closeable?: boolean;
-    }>(),
-    {
-        show: false,
-        maxWidth: '2xl',
-        closeable: true,
+const props = defineProps({
+    show: {
+        type: Boolean,
+        default: false,
     },
-);
+    maxWidth: {
+        type: String,
+        default: '2xl',
+    },
+    closeable: {
+        type: Boolean,
+        default: true,
+    },
+});
 
 const emit = defineEmits(['close']);
 
@@ -22,7 +24,7 @@ watch(
         if (props.show) {
             document.body.style.overflow = 'hidden';
         } else {
-            document.body.style.overflow = 'visible';
+            document.body.style.overflow = null;
         }
     },
 );
@@ -33,7 +35,7 @@ const close = () => {
     }
 };
 
-const closeOnEscape = (e: KeyboardEvent) => {
+const closeOnEscape = (e) => {
     if (e.key === 'Escape' && props.show) {
         close();
     }
@@ -43,7 +45,7 @@ onMounted(() => document.addEventListener('keydown', closeOnEscape));
 
 onUnmounted(() => {
     document.removeEventListener('keydown', closeOnEscape);
-    document.body.style.overflow = 'visible';
+    document.body.style.overflow = null;
 });
 
 const maxWidthClass = computed(() => {
@@ -79,7 +81,7 @@ const maxWidthClass = computed(() => {
                         @click="close"
                     >
                         <div
-                            class="absolute inset-0 bg-gray-500 opacity-75 dark:bg-gray-900"
+                            class="absolute inset-0 bg-gray-500 opacity-75"
                         />
                     </div>
                 </Transition>
@@ -94,7 +96,7 @@ const maxWidthClass = computed(() => {
                 >
                     <div
                         v-show="show"
-                        class="mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full dark:bg-gray-800"
+                        class="mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full"
                         :class="maxWidthClass"
                     >
                         <slot v-if="show" />
