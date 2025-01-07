8469cbd2124ff7e462190293833de9ca648935c4
MIGUEL MORT <devmigueluicab@gmail.com>
2025-01-07 10:55:24 -0600

    RJXM

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ __('RJXM') }}</title>
     <meta name="description" content="{{ 'Dirigencia Juvenil del PRI' }}">
-    
+    <meta property="og:image" content="{{ asset('assets/img/default-og-image.jpg') }}">
+
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
